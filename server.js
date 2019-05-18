79b2e682edda559250c10be459478ecdaa092569
Aneerudh <aneerudh.p@mrcooper.com>
2019-05-17 22:16:46 -0700

    Cleanup front-end code

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 const colors = require('colors');
 const app = require('./app/app');
 
-// const server = app.listen(config.port); -- Fix this soon.
 const PORT = process.env.PORT || 8090;
 app.listen(PORT, () => startUp());
 
 const startUp = () => {
-  console.log(colors.green(`INFO::server started on port ${PORT}`)); // eslint-disable-line no-console, max-len
+  // eslint-disable-next-line no-console
+  console.log(colors.green(`INFO::server started on port ${PORT}`));
 };
 
 const shutDown = () => {
-  console.log(colors.red('************ Shutting Down Server **************')); // eslint-disable-line no-console, max-len
+  // eslint-disable-next-line no-console
+  console.log(colors.red('************ Shutting Down Server **************'));
 };
 
 process.once('SIGINT', shutDown);
